3a21a4d80d664ef4111f1ba741b6f0096b4bc62d
Emmano89 <71134499+Emmano89@users.noreply.github.com>
2021-01-10 03:24:59 +0100

    Update Estimation_PI.py
    Calcul de pi et du temps d'exécution

--- Estimation_PI.py
+++ Estimation_PI.py
@@ -6,11 +6,39 @@
 # =============================================================================
 
 from random import *
+import findspark
+findspark.init("C:/spark")
+import pyspark
+import random
+from pyspark import SparkContext, SparkConf
+from pyspark.sql import SparkSession
+from time import time
+import numpy as np
+from operator import add
 
-def is_point_inside_unit_circle():
+conf = SparkConf().setAppName("PI").setMaster("local")
+sc = SparkContext(conf=conf)
+
+def is_point_inside_unit_circle(p):
  x, y = random(), random()
  return 1 if x*x + y*y < 1 else 0
 
-nin = is_point_inside_unit_circle()
-#nin permet de determiner si le point selectionner est dans le cercle où pas, le résultat affiche 1 si oui 0 sinon
+inside = is_point_inside_unit_circle()
+#inside permet de determiner si le point selectionné est dans le cercle où pas, le résultat affiche 1 si oui 0 sinon
+## Calcul de pi
+count = sc.parallelize(range(0, num_samples)).filter(inside).count()
+
+pi = 4 * count / num_samples
+print(pi)
+
+sc.stop()
+### Calcul du temps d'exécution 
+t_0 = time()
+
+
+count = sc.parallelize(range(0, num_samples)) \
+             .map(inside).reduce(add)
+print(np.round(time()-t_0, 3), "temps d'exécution avec n=", num_samples)
+print("Pi is roughly %f" % (4.0 * count / num_samples))
+
 
